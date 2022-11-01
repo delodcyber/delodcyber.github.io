ffa107d313aceb178f2c235b69a0e847894cc068
delodcyber <truebasisoflife@yahoo.com>
2022-11-01 15:51:14 +0100

    initial commit

---
+++ chamber/scripts/script.js
@@ -0,0 +1,5 @@
+let date = new Date();
+let year = date.getFullYear();
+// document.getElementById("year").textContent = year;
+let modified = document.lastModified;
+document.getElementById("modified").textContent = modified;
