6d6785e45062546f949dcc021266a23a354fa172
delodcyber <truebasisoflife@yahoo.com>
2022-11-08 09:53:28 +0100

    initial commit

---
+++ lesson05/bom.js
@@ -0,0 +1,24 @@
+const button = document.querySelector('button');
+const input = document.querySelector('#favchap');
+const output = document.querySelector('.list');
+
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        let li = document.createElement('li');
+        li.textContent = input.value;
+
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'x';
+
+        li.append(deleteButton);
+        output.append(li);
+
+        deleteButton.addEventListener('click', function() {
+            output.removeChild(li);
+            input.focus;
+        });
+        input.value = '';
+        input.focus; 
+        console.log()   
+    }
+})
