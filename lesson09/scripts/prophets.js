1adcbbf0eb869643131f5bc7349985c9c5e5d05d
delodcyber <truebasisoflife@yahoo.com>
2022-12-03 14:25:25 +0100

    initial commit

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,4 +1,4 @@
-const requestURL = 'http://delodcyber.github.io/lesson09/latter-day-prophets.json';
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
 
 fetch(requestURL)
